c66acf58e784f03202dbab9e0618ae0efc417c88
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-21 20:16:21 -0300

    feat(PendingAccount): add querys in jpa repository

--- src/main/java/br/com/coderbank/customerportal/repository/PendingAccountRepository.java
+++ src/main/java/br/com/coderbank/customerportal/repository/PendingAccountRepository.java
@@ -1,9 +1,17 @@
 package br.com.coderbank.customerportal.repository;
 
 import br.com.coderbank.customerportal.entity.PendingAccount;
+import br.com.coderbank.customerportal.enuns.AccountStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PendingAccountRepository extends JpaRepository<PendingAccount, UUID> {
+
+    List<PendingAccount> findByAccountStatus(AccountStatus accountStatus);
+
+    boolean existsByCustomerId(UUID id);
+
+    PendingAccount findByCustomerId(UUID id);
 }
